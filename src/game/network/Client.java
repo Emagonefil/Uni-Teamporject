6ea7c90cfca18c91b91b6bc98ac2aca1c79146a4
Feilue Lin <fxl724@student.bham.ac.uk>
2019-01-23 19:56:06 +0000

    separate the network part from ui

--- src/game/network/Client.java
+++ src/game/network/Client.java
@@ -1,132 +1,48 @@
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
+package goldenaxe.network.tcp;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.BlockingQueue;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.KeyStroke;
-
-public class Client extends JFrame implements KeyListener{
-
-	static Client client;
-	static HashMap<Integer,Plane> planes = new HashMap<Integer,Plane>();
+public class Client {
 	
-	static Socket server = null;
-	static PrintStream toServer = null;
-	static ObjectInputStream fromServer = null;
-	
-	
-	BufferedImage bg;
-	BufferedImage plane;
-	
-	public void paint(Graphics g) {
-		g.drawImage(bg,0,0,null);
-		if(!planes.isEmpty()) {
-			for(Map.Entry<Integer, Plane> m : planes.entrySet()) {
-				Plane p = m.getValue();
-				g.drawString(String.valueOf(m.getKey()), p.getX()+15, p.getY()-20);
-				g.drawImage(plane, p.getX(),p.getY(),null);
-			}
-		}
-	}
+	private Socket server;
+	private PrintStream toServer = null;
+	private ObjectInputStream fromServer = null;
 	
 	
 	
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		client = new Client();
-		server = new Socket("192.168.191.1", 6140);
-		
-		toServer = new PrintStream(server.getOutputStream());
-		fromServer = new ObjectInputStream(server.getInputStream());
-		new Thread(new Receiver()).start();
-	}
-	
-	public Client() {
+	public Client(String ip, Integer port) {
 		try {
-			bg = ImageIO.read(new File("src/img/bg.jpg"));
-			plane = ImageIO.read(new File("src/img/plane.png"));
+			System.out.println("Connecting to Server....");
+			server = new Socket(ip, port);
+			System.out.println("Connected");
+			toServer = new PrintStream(server.getOutputStream());
+			fromServer = new ObjectInputStream(server.getInputStream());
+			System.out.println("Client is ready");
+		} catch (UnknownHostException e) {
+			System.out.println("Cannot connect to server");
+			System.exit(1);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		addKeyListener(this);
-		this.setTitle("plane.io");
-		
-		this.setSize(bg.getWidth(),bg.getHeight());
-		this.setVisible(true);
 	}
 	
-	private static class Receiver implements Runnable{
-
-		
-		@Override
-		public void run() {
-			try {
-				while(true) {
-					planes = (HashMap<Integer, Plane>) fromServer.readObject();
-					
-					client.repaint();
-					
-				}
-			}catch(Exception e) {
-				
-			}
-			
-		}
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int i = e.getKeyCode();
-		
-		if(i == KeyEvent.VK_UP) {
-			toServer.println("forward");
-		}
-		if(i == KeyEvent.VK_DOWN){
-			toServer.println("backward");
-		}
-		if(i == KeyEvent.VK_LEFT) {
-			toServer.println("left");
-		}
-		if(i == KeyEvent.VK_RIGHT){
-			toServer.println("right");
-		}
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	//send String str to server
+	public void send(String str) {
+		toServer.println(str);
 	}
 	
-
-
+	//return the object when receive something
+	public Object receive() throws ClassNotFoundException, IOException{
+		return fromServer.readObject();
+	}
 	
+	
+		
 	
 	
 }
--- src/game/network/Server.java
+++ src/game/network/Server.java
@@ -1,111 +1,34 @@
-import java.util.List;
-import java.util.Random;
-import java.io.BufferedReader;
+package goldenaxe.network.tcp;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+
+import goldenaxe.network.tcp.thread.ThreadPool;
+
 
 public class Server {
 
+	private ServerSocket serverSocket;
+	private ThreadPool threadPool;
 	
-	public static AllPlanes allPlanes = new AllPlanes();
 	
-	public static void main(String[] args) {
-		Random r = new Random();
+	public Server(Integer port) {
 		try {
-			ServerSocket serverSocket = new ServerSocket(6140);
-			while(true) {
-				Socket socket = serverSocket.accept();
-				new Thread(new ServerSender(socket)).start();
-				new Thread(new ServerReceiver(socket)).start();
-				allPlanes.add(socket.getPort(),new Plane(r.nextInt(700),r.nextInt(700)));
-			}
+			System.out.println("Server Starting....");
+			serverSocket = new ServerSocket(port);
+			System.out.println("Server Started !");
+			acceptClients();
+			System.out.println("Clients are availabe to connect now");
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private static class ServerReceiver implements Runnable{
-
-		Socket socket = null;
-		BufferedReader fromClient = null;
-		
-		public ServerReceiver(Socket socket) throws IOException {
-			this.socket = socket;
-			this.fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		}
-		
-		@Override
-		public void run() {
-			String userInput;
-			try {
-				while(true) {
-					userInput = fromClient.readLine();
-					if(userInput.equals("forward")) {
-						forward(socket);
-					}else if(userInput.equals("backward")) {
-						backward(socket);
-					}else if(userInput.equals("left")) {
-						left(socket);
-					}else if(userInput.equals("right")) {
-						right(socket);
-					}
-					
-					
-				}
-			} catch (Exception e) {
-				System.out.println(socket.getPort()+" logout");
-				allPlanes.die(socket.getPort());
-			}
-			
-		}
-		
-	}
 	
-	private static class ServerSender implements Runnable{
-		
-		Socket socket = null;
-		ObjectOutputStream toClient = null;
-		
-		public ServerSender(Socket socket) throws IOException {
-			this.socket = socket;
-			this.toClient = new ObjectOutputStream(socket.getOutputStream());
-		}
-		
-		public void run() {
-			try {
-				
-				while(true) {
-					toClient.writeObject(allPlanes.getAll());
-					toClient.reset();
-				}
-			}catch(Exception e){
-				
-			}
-		}
+	private void acceptClients() throws IOException {
+		serverSocket.accept();
+		//TODO  add threads ...
 	}
-	
-	private static void forward(Socket socket) {
-		Plane plane = allPlanes.getPlane(socket.getPort());
-		plane.forward();
-	}
-	private static void backward(Socket socket) {
-		Plane plane = allPlanes.getPlane(socket.getPort());
-		plane.backward();
-	}
-	private static void left(Socket socket) {
-		Plane plane = allPlanes.getPlane(socket.getPort());
-		plane.left();
-	}
-	private static void right(Socket socket) {
-		Plane plane = allPlanes.getPlane(socket.getPort());
-		plane.right();
-	}
-	
 }
