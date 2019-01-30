72182bff1542a31044b35c814bac6036dbeb9599
Callum <cxa657@student.bham.ac.uk>
2019-01-30 13:44:42 +0000

    Finished Collision Detection Needs Testinggit add *.java!

--- src/game/entity/collisions/CollisionDetection.java
+++ src/game/entity/collisions/CollisionDetection.java
@@ -1,6 +1,7 @@
 package game.entity.collisions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import game.entity.Point;
 
@@ -27,7 +28,7 @@ public class CollisionDetection {
 
 	// Note: it is assumed that corners are connected to the
 	// corner before and after them in a circular fashion
-	private static void getAxis(Point[] corners,ArrayList<Vector2d> axis) {
+	private static void getAxis(Point[] corners, ArrayList<Vector2d> axis) {
 		for (int i = 0; i < corners.length; i++) {
 			Vector2d normal;
 			if (i == corners.length - 1) {
@@ -45,9 +46,38 @@ public class CollisionDetection {
 	public static boolean isTouching(Point[] c1, Point[] c2) {
 		ArrayList<Vector2d> axis = new ArrayList<Vector2d>();
 		// Get the unique normals of each face to use as axis:
-		getAxis(c1,axis);
-		getAxis(c2,axis);
+		getAxis(c1, axis);
+		getAxis(c2, axis);
 
+		for (Vector2d ax : axis) {
+			float c1Min;
+			float c1Max;
+			float c2Min;
+			float c2Max;
+
+			ArrayList<Float> projections = new ArrayList<Float>();
+			// Using dot with axis to project point onto axis
+			for (Point p : c1) {
+				Vector2d pointVector = new Vector2d(p.getX(), p.getY());
+				projections.add(Vector2d.dot(pointVector, ax));
+			}
+
+			c1Min = Collections.min(projections);
+			c1Max = Collections.max(projections);
+
+			projections.clear();
+			for (Point p : c2) {
+				Vector2d pointVector = new Vector2d(p.getX(), p.getY());
+				projections.add(Vector2d.dot(pointVector, ax));
+			}
+			
+			c2Min = Collections.min(projections);
+			c2Max = Collections.max(projections);
+			
+			if (c1Min>c2Max || c2Min>c1Max) {
+				return false;
+			}
+		}
 		return true;
 	}
 
