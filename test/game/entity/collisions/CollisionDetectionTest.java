cbad481a87a7bd96d9992353a381943ac0cb22dd
Callum <cxa657@student.bham.ac.uk>
2019-02-01 11:21:12 +0000

    Basic collision detection tests

--- test/game/entity/collisions/CollisionDetectionTest.java
+++ test/game/entity/collisions/CollisionDetectionTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import game.entity.Point;
+import game.entity.RectangularEntity;
+
 public class CollisionDetectionTest {
 
+	//TO-DO add more testing.
 	@Test
 	public void testIsTouching() {
-		fail("Not Implemented");
+		RectangularEntity r1 = new RectangularEntity(10, 10, new Point(5, 5));
+		RectangularEntity r2 = new RectangularEntity(10, 10, new Point(3,3));
+		RectangularEntity r3 = new RectangularEntity(2, 2, new Point(100, 100));
+		assert(CollisionDetection.isTouching(r1.getCorners(), r2.getCorners()));
+		assert(!(CollisionDetection.isTouching(r1.getCorners(), r3.getCorners())));
 	}
 
 }
