e6d9cb6e8c0660c009f683c1b71f2fb3c8586c4f
Andreea Bila <aib743@cs.bham.ac.uk>
2019-03-07 00:23:09 +0000

    Revert "provisional commit"
    This reverts commit e2b8f681918090542ebe2c43cfb0b906f25d5e85

--- src/game/entity/Entity.java
+++ src/game/entity/Entity.java
@@ -1,7 +1,5 @@
 package game.entity;
 
-import javafx.scene.image.Image;
-
 import java.io.Serializable;
 
 public class Entity implements IEntity, Serializable {
@@ -14,7 +12,6 @@ public class Entity implements IEntity, Serializable {
 	public String type;
 	public float width;
 	public float height;
-	public Image image;
 	// Dont use this
 	public Entity() {
 		this.position = new Point(0.0f, 0.0f);
