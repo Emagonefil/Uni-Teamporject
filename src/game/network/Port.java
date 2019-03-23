fbb01d89cb7987b227a81c0da0eeaa1d3778ad40
Andreea Bila <andreeabila@yahoo.com>
2019-03-23 16:52:51 +0000

    not much

--- src/game/network/Port.java
+++ src/game/network/Port.java
@@ -6,8 +6,8 @@ public class Port {
 	public static final int serverPort = 7777;
 	public static String serverAddress = "127.0.0.1";
 	public static String mulitcastAddress = "230.0.1.1";
-//	public static String roomServerAddress="192.168.137.1";
-	public static String roomServerAddress="127.0.0.1";
+	public static String roomServerAddress="192.168.137.1";
+//	public static String roomServerAddress="127.0.0.1";
 	public static String roomPort="9999";
 
 	public static String localIP = null;
